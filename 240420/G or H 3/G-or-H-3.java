595885c6e642cc32cc478e92facc71e6d0a78b0a
BaekSeungJin_백승진 <99312529+BaxDailyGit@users.noreply.github.com>
2024-04-20 20:53:21 +0900

    Title: G or H 3, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240420/G or H 3/G-or-H-3.java
+++ 240420/G or H 3/G-or-H-3.java
@@ -14,7 +14,7 @@ public class Main {
         }
 
         // 각 위치에 점수 등록
-        int[] placed = new int[10000];
+        int[] placed = new int[10001];
         for(int i = 1; i < N + 1; i++) {
             if(alpha[i] == 'G') {
                 placed[arr[i]] = 1;
@@ -32,11 +32,11 @@ public class Main {
         
         // 순회하며 사진을 찍어 얻을 수 있는 최대 점수 찾기        
         int maxSum = 0;
-        for(int i = 1; i < max; i++) {
-            //System.out.println("i: "+i);
+        for(int i = 1; i < max - K + 1; i++) {
+            System.out.println("i: "+i);
             int sum = 0;
-            for(int j = i; j < i + K + 1; j++) {
-                //System.out.println("    placed["+j+"]: "+placed[j]);
+            for(int j = i; j <= i + K; j++) {
+                System.out.println("    placed["+j+"]: "+placed[j]);
                 sum += placed[j];
             }
             maxSum = Math.max(maxSum, sum);
