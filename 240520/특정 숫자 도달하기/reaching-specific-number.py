73ba296f2cb6d76ba99a5290811a4fbab5b85200
BaekSeungJin_백승진 <99312529+BaxDailyGit@users.noreply.github.com>
2024-05-20 23:12:08 +0900

    Title: 특정 숫자 도달하기, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240520/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,14 @@
+arr = list(map(int, input().split()))
+
+sum_val = 0 
+# arr의 원소의 인덱스를 생성
+for i in range(len(arr)):
+    #해당 인덱스의 값이 250이 넘으면 
+    if arr[i] < 250:
+        sum_val += arr[i]
+    else:
+        break
+print(sum_val, sum_val/i)
+        
+#     else:
+#         print(sum_val, sum_val/i-1)
