e09290acd9d76d0b7296d068ad013f4fc57f9b6b
BaekSeungJin_백승진 <99312529+BaxDailyGit@users.noreply.github.com>
2024-06-08 17:19:24 +0900

    Title: 특정 구간의 원소 평균값, Time: 178ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240608/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] arr = new double[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextDouble();
+        }
+
+        // 구간 정해서 구간에 있는 원소들을 모두 더해서 평균 구하고 
+        // 그 구간안에 동일한 원소가 있으면 cnt++
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            //System.out.println("-----------");
+            for(int j = i; j < n; j++) {
+                double sumEle = 0;
+                double cntEle = 0;
+                for(int k = i; k <= j; k++) {
+                    sumEle += arr[k];
+                    cntEle++;
+                }
+                double avg = sumEle / cntEle;
+                //System.out.println(avg + " = " + sumEle + "/" + cntEle);
+                for(int k = 0; k < n; k++) {
+                    if(avg == arr[k]) {
+                        //System.out.println("    " + avg + " == " + arr[k]);
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
