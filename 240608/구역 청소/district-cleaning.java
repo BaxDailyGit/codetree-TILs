abf2d112de6acd9e3bfa1c662f90d78e63bfe101
BaekSeungJin_백승진 <99312529+BaxDailyGit@users.noreply.github.com>
2024-06-08 22:26:26 +0900

    Title: 구역 청소, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240608/구역 청소/district-cleaning.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int[] arr = new int[101];
+        for(int i = a; i <= b; i++) {
+            arr[i] = 1;
+        }
+        for(int i = c; i <= d; i++) {
+            arr[i] = 1;
+        }
+        int sum = 0;
+        for(int i = 1; i <= 100; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum-1);
+    }
+}
