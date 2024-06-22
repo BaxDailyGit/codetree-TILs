eee7cc5208d1f7284230afe8f16b1ef54f07d1b5
BaekSeungJin_백승진 <99312529+BaxDailyGit@users.noreply.github.com>
2024-06-23 01:23:44 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240623/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.Stack;
+public class Main {
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.next();
+
+        // 스택을 활용한 괄호 적합성 판단
+        Stack<Character> s = new Stack<>();
+        for(int i = 0; i < inputStr.length(); i++) {
+            
+            if(inputStr.charAt(i) == '(') {
+                s.push(inputStr.charAt(i));
+            } else if (inputStr.charAt(i) == ')') {
+                s.pop();
+            } else {
+                System.out.println("No");
+            }
+            
+        }
+        if(s.empty() == false) {
+            System.out.println("No");
+        } else {
+            System.out.println("Yes");
+        }
+    }
+}
